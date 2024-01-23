d02b9c25a4bceccd336ad142aff143284cfdea7d
David0770 <davidgomesgc230@gmail.com>
2024-01-23 12:11:22 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import router from './routes/routes'
+import 'dotenv/config'
+
+const app = express()
+
+app.use(express.json())
+app.use(router)
+
+
+app.listen(process.env.PORT || 3000)
